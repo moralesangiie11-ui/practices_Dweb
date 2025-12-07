0007688c3cc0f8bd86f93516ef36e8e87c43bda7
moralesangiie11-ui <moralesangiie11@gmail.com>
2025-12-06 21:38:19 -0500

    Update run.py

--- mi_proyecto_peliculas/run.py
+++ mi_proyecto_peliculas/run.py
@@ -1,7 +1,6 @@
-# run.py
 
 from dotenv import load_dotenv
-# Asegúrate de que esta línea esté correcta
+
 from app import create_app 
 
 load_dotenv() 
@@ -9,4 +8,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
+
     app.run(debug=True, port=8002)
